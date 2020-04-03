69d2341e53b4e6703cc646a0470efcc34884bd1e
wlukla <vladislav.danilochkin@gmail.com>
2020-04-03 04:55:05 +0300

    feat: add put, post and delete routes for boards

--- src/resources/boards/board.router.js
+++ src/resources/boards/board.router.js
@@ -1,15 +1,33 @@
 const router = require('express').Router();
-// const Task = require('./task.model');
+const Board = require('./board.model');
 const boardService = require('./board.service');
 
-router.route('/').get((req, res) => {
-  const tasks = boardService.getAll();
-  res.json(tasks);
-});
+router
+  .route('/')
+  .get((req, res) => {
+    const tasks = boardService.getAll();
+    res.json(tasks);
+  })
+  .post((req, res) => {
+    const newBoard = { ...new Board(), ...req.body };
+    boardService.add(newBoard);
 
-router.route('/:boardID').get((req, res) => {
-  const board = boardService.getByID(req.params.boardID);
-  res.json(board);
-});
+    res.send('The board has been created.');
+  });
+
+router
+  .route('/:boardID')
+  .get((req, res) => {
+    const board = boardService.getByID(req.params.boardID);
+    res.json(board);
+  })
+  .put((req, res) => {
+    boardService.update(req.params.boardID, req.body);
+    res.send('The board has been updated.');
+  })
+  .delete((req, res) => {
+    boardService.deleteBoard(req.params.boardID);
+    res.send('The board has been deleted.');
+  });
 
 module.exports = router;
--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,15 +1,33 @@
 const boardRepo = require('./board.memory.repository');
 
 const boards = boardRepo.getAll();
-console.log(boards);
 
 const getAll = () => boards;
 const getByID = id => {
   const res = boards.find(board => board.id === id);
   return res;
 };
+const add = board => {
+  boards.push(board);
+};
+
+const update = (id, data) => {
+  const boardIdx = boards.map(board => board.id).indexOf(id);
+  boards[boardIdx] = { ...boards[boardIdx], ...data };
+};
+
+const deleteBoard = id => {
+  console.log(id);
+  const boardIdx = boards.map(board => board.id).indexOf(id);
+  if (boardIdx > -1) {
+    boards.splice(boardIdx, 1);
+  }
+};
 
 module.exports = {
   getAll,
-  getByID
+  getByID,
+  add,
+  update,
+  deleteBoard
 };
