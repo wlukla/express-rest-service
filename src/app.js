df9e45ec3bd4fbff59520d2cfc616d97a38d99ff
wlukla <vladislav.danilochkin@gmail.com>
2020-04-12 15:15:54 +0300

    feat: add unhandledRejection and uncaughtException handling

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
 
-const logger = require('./logger');
+const { requestLogger, errorLogger } = require('./logger');
 
 const userRouter = require('./resources/users/user.router');
 const taskRouter = require('./resources/tasks/task.router');
@@ -14,9 +14,9 @@ const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
+app.use(requestLogger);
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
     res.send('Service is running!');
@@ -25,10 +25,10 @@ app.use('/', (req, res, next) => {
   next();
 });
 
-app.use(logger);
-
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/', taskRouter);
+
+app.use(errorLogger);
 
 module.exports = app;
--- src/logger/index.js
+++ src/logger/index.js
@@ -1,23 +1,33 @@
-const morgan = require('morgan');
+/* eslint-disable no-unused-vars */
+const winston = require('winston');
 
-morgan.token('query', req => {
-  return JSON.stringify(req.query);
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'combined.log', flags: 'a' })
+  ],
+  format: winston.format.cli()
 });
 
-morgan.token('body', req => {
-  return JSON.stringify(req.body);
-});
+const requestLogger = (req, res, next) => {
+  const { method, url, params, body } = req;
+  logger.log(
+    'info',
+    `${method} url: ${url} params: ${JSON.stringify(
+      params
+    )} body: ${JSON.stringify(body)}`
+  );
+  next();
+};
 
-const logger = morgan((tokens, req) =>
-  [
-    tokens.method(req),
-    'URL:',
-    tokens.url(req),
-    'query:',
-    tokens.query(req),
-    'body:',
-    tokens.body(req)
-  ].join(' ')
-);
+const errorLogger = (err, req, res, next) => {
+  const { statusCode, message } = err;
+  res.status(500).send(message);
+  logger.log('error', `CODE 500: ${message}`);
+};
 
-module.exports = logger;
+const processLogger = (err, message) => {
+  logger.log('error', `${err}: ${message}`);
+};
+
+module.exports = { requestLogger, errorLogger, processLogger };
