b092fe3757ba1610be9578c41f9bdab1de9fea5c
wlukla <vladislav.danilochkin@gmail.com>
2020-04-19 07:40:50 +0300

    feat: add db connecting

---
+++ src/db/db.client.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectToDB = callback => {
+  mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', () => {
+    console.log('Database connected!');
+    callback();
+  });
+};
+
+module.exports = connectToDB;
